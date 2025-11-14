10f18485b5441d604a9f141433716e2b01f2d822
Basan <basan.info.2012@gmail.com>
2025-11-14 21:48:15 +0300

    Initial commit

---
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -0,0 +1,95 @@
+package com.techie.microservices.order;
+
+import com.techie.microservices.order.event.OrderPlacedEvent;
+import com.techie.microservices.order.stubs.InventoryClientStub;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
+class OrderServiceApplicationTests {
+
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+    @MockBean
+    private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
+
+    @BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+    @WithMockUser(username = "user")
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1,
+                     "userDetails" : {
+                        "email" : "test@gmail.com"
+                     }
+                }
+                """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
+        Mockito.when(kafkaTemplate.send(anyString(), any())).thenReturn(null);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
+
+	@Test
+	void shouldFailOrderWhenProductIsNotInStock() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1000
+                }
+                """;
+		InventoryClientStub.stubInventoryCall("iphone_15", 1000);
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(500);
+	}
+}
